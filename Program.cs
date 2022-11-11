b68495adfb02656e20c0f289a25710e6bac84762
Reynald Albuquerque Costa <Reynald Albuquerque Costa>
2022-11-11 00:25:42 -0300

    @Reynald-Albuquerque-Costa[Desafio concluído !]

--- Program.cs
+++ Program.cs
@@ -1,3 +1,14 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia(numero: "51165", modelo: "Modelo 1", imei: "33333333", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Instagram");
+
+
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone(numero: "84y843", modelo: "Modelo 8", imei: "99999999", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Facebook");
+
